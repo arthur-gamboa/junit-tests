80dbfd0faa55c0e9586954a13893a314697a34af
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-22 15:42:35 -0600

    added test field and grade structure

---
+++ src/main/java/Student.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    @Test
+    public void testIfStudentObjectCanBeCreated() {
+        Student john = new Student(1L, "John");
+        Student david = null;
+//        System.out.println(john);
+        assertNotNull(john);
+        assertNull(david);
+    }
+
+    @Test
+    public void testIfFieldWork() {
+        Student john = new Student(1L, "John");
+        assertEquals(1, john.getId());
+        assertEquals("John", john.getName());
+        assertNotNull(john.getGrades());
+
+        Student david = new Student(2L, "David");
+        assertNotEquals("Random", david.getName());
+    }
+
+    @Test
+    public void tesIfAddGradeWorks() {
+        Student john = new Student(1L, "John");
+        assertEquals(0, john.getGrades().size());
+        john.addGrade(100);
+        assertEquals(1, john.getGrades().size());
+        john.addGrade(70);
+        assertEquals(2, john.getGrades().size());
+
+//        john.getGrades().get(0);
+        assertEquals(100, (int) john.getGrades().get(0));
+        assertEquals(70, (int) john.getGrades().get(1));
+    }
+
+}
